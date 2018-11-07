8c662d47236b1b352da4c006b94f70bb3c5cc65d
Raphaël Marzat <raphael.marzat@outlook.com>
2018-11-07 13:43:18 +0000

    Question1 code finished

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,12 @@
 #lang racket
+;Cons pair of 2 numbers
+(cons 1 2)
+;List of 3 numbers built with the cons function
+(cons 3 (cons 4 (cons 5 empty)))
+;List containing a String, a number and a nested list of three numbers (cons function)
+(cons "cons" (cons 6 (cons '(7 8 9) empty)))
+;List containing a String, a number and a nested list of three numbers (list function)
+(list "list" 10 '(11 12 13))
+;List containing a String, a number and a nested list of three numbers (append function)
+(append '("append") '(14) '((15 16 17)))
+
